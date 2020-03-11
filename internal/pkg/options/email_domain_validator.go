c957a28150e678ff60f9e595ddc3d1cb446fb6e6
Jusshersmith <usshersmithj@gmail.com>
2020-03-11 12:04:18 +0000

    some quick additions to test potential logic

--- internal/pkg/options/email_domain_validator.go
+++ internal/pkg/options/email_domain_validator.go
@@ -25,19 +25,13 @@ type EmailDomainValidator struct {
 // - is non-empty
 // - the domain of the email address matches one of the originally passed in domains.
 //   (case insensitive)
-// - if the originally passed in list of domains consists only of "*", then all emails
-//   are considered valid based on their domain.
 // If valid, nil is returned in place of an error.
 func NewEmailDomainValidator(allowedDomains []string) EmailDomainValidator {
 	emailDomains := make([]string, 0, len(allowedDomains))
 
 	for _, domain := range allowedDomains {
-		if domain == "*" {
-			emailDomains = append(emailDomains, domain)
-		} else {
-			emailDomain := fmt.Sprintf("@%s", strings.ToLower(domain))
-			emailDomains = append(emailDomains, emailDomain)
-		}
+		emailDomain := fmt.Sprintf("@%s", strings.ToLower(domain))
+		emailDomains = append(emailDomains, emailDomain)
 	}
 	return EmailDomainValidator{
 		AllowedDomains: emailDomains,
@@ -51,10 +45,6 @@ func (v EmailDomainValidator) Validate(session *sessions.SessionState) error {
 
 	if len(v.AllowedDomains) == 0 {
 		return ErrEmailDomainDenied
-	}
-
-	if len(v.AllowedDomains) == 1 && v.AllowedDomains[0] == "*" {
-		return nil
 	}
 
 	err := v.validate(session)
--- internal/proxy/options.go
+++ internal/proxy/options.go
@@ -64,6 +64,7 @@ type Options struct {
 	DefaultAllowedEmailDomains   []string `envconfig:"DEFAULT_ALLOWED_EMAIL_DOMAINS"`
 	DefaultAllowedEmailAddresses []string `envconfig:"DEFAULT_ALLOWED_EMAIL_ADDRESSES"`
 	DefaultAllowedGroups         []string `envconfig:"DEFAULT_ALLOWED_GROUPS"`
+	AllowNoValidators            bool     `envconfig:"ALLOW_NO_VALIDATORS" default:"false"`
 
 	ClientID     string `envconfig:"CLIENT_ID"`
 	ClientSecret string `envconfig:"CLIENT_SECRET"`
@@ -196,20 +197,30 @@ func (o *Options) Validate() error {
 	}
 
 	if o.upstreamConfigs != nil {
-		invalidUpstreams := []string{}
+		noValidatorsDefined := []string{}
+		wildcardUsed := []string{}
 		for _, uc := range o.upstreamConfigs {
 			if uc.Timeout > o.TCPWriteTimeout {
 				o.TCPWriteTimeout = uc.Timeout
 			}
-
-			if len(uc.AllowedEmailDomains) == 0 && len(uc.AllowedEmailAddresses) == 0 && len(uc.AllowedGroups) == 0 {
-				invalidUpstreams = append(invalidUpstreams, uc.Service)
+			if !o.AllowNoValidators {
+				if len(uc.AllowedEmailDomains) == 0 && len(uc.AllowedEmailAddresses) == 0 && len(uc.AllowedGroups) == 0 {
+					noValidatorsDefined = append(noValidatorsDefined, uc.Service)
+				} else if uc.AllowedEmailDomains[0] == "*" || uc.AllowedEmailAddresses[0] == "*" || uc.AllowedGroups[0] == "*" {
+					wildcardUsed = append(wildcardUsed, uc.Service)
+				}
 			}
 		}
-		if len(invalidUpstreams) != 0 {
+		if len(noValidatorsDefined) != 0 {
 			msgs = append(msgs, fmt.Sprintf(
-				"missing setting: ALLOWED_EMAIL_DOMAINS, ALLOWED_EMAIL_ADDRESSES, ALLOWED_GROUPS default in environment or override in upstream config in the following upstreams: %v",
-				invalidUpstreams))
+				`missing setting: ALLOWED_EMAIL_DOMAINS, ALLOWED_EMAIL_ADDRESSES, ALLOWED_GROUPS default in environment or override in upstream config. 
+				If no extra validators are required, set 'ALLOW_NO_VALIDATORS' to 'true'. Affected usptreams: %v`,
+				noValidatorsDefined))
+		}
+		if len(wildcardUsed) != 0 {
+			msgs = append(msgs, fmt.Sprintf(
+				"invalid setting: a wildcard cannot be used within validators. If no extra validators are required, set 'ALLOW_NO_VALIDATORS' to 'true'. Affected upstreams: %v",
+				wildcardUsed))
 		}
 	}
 
